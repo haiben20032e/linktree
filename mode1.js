e2b17d309dbd58c67b4634c9ec6c97132047d547
haiben20032e <doanhai11903@gmail.com>
2025-05-25 00:22:43 +0700

    Cập nhật code mới

--- mode1.js
+++ mode1.js
@@ -1,11 +1,11 @@
 const diaryEntries = [
     ` 2025 / 1 ngày buồn nào đó
-    Tớ Ghét bản thân tớ , mình chỉ đang cố hằng ngày thức dậy đối mặt với chính bản thân mình chỉ đang cố gắng nỗ lực chứng minh bản thân 
+    Tớ Ghét bản thân tớ , mình chỉ đang cố hằng ngày thức dậy đối mặt với chính bản thân mình chỉ đang cố gắng nỗ lực chứng minh bản thân , tớ chỉ luôn cố gắng làm tốt mọi thứ , nhưng quên cả bản thân mình đang hướng đến điều gì mà lại để đánh mắt thứ quan trọng nhất
     Chắc tại do mình hướng nội , luôn luôn làm gì cũng chỉ biết tránh né , nói cảm ơn và xin lỗi liên tục vì sợ ảnh hưởng đến người khác 
     Gặp được người mình yêu hiểu mình nhưng lại tránh né , họ quan tâm nhưng sợ mình không xứng , nỗi tự ti của mình quá lớn chăng , bước vào một mối quan hệ mình đã chỉ để ý đến nhưng thứ tồi tệ nhất , đôi khi mọi thứ thật bình thường nhưng 1 khoảnh khắc tớ làm cậu buồn tớ chợt nghĩ mình không xứng , tự dằn vặt về điều mình đã làm , có lẽ tớ đã vụn vỡ từ những lần trước đó khi mà ng ta bỏ rơi tớ ,lòng tôi vụn vỡ mất 3 năm nó thành 1 vết sẹo , một ám ảnh về tình yêu tự trách 
     Có lẽ cậu nghĩ tớ rời đi vì "xa" nhưng nếu cần tớ vẫn sẽ tới và ôm cậu thật chặt , xa chưa bao giờ là vấn đề cả tớ còn muốn gặp cậu rất nhiều nữa chứ, cậu là 1 cô gái tốt 1 người hiểu tớ chấp nhận tớ, mọi người bạn bè đạt ấy à nói về cậu tớ chưa bao giờ để ý cả , tớ yêu cậu trong mắt tớ cậu là đẹp nhất đâu quan trọng ng khác nói gì , như ng ta đã nói khi yêu 1 ai họ luôn đẹp nhất , đáng yêu trong mắt của mình - yêu cậu từ 1 khoảnh khắc cơ mà ,cậu vẫn luôn hỏi đó là khoảnh khắc gì ,nó đơn giản lắm "em ăn , em nói ,em cười , thế gian đâu tìm được 2 người như em " chỉ là khoảnh khắc cậu bỗng cười ,cáu bẩn , hay gặp 1 thứ gì đó cute chia sẻ cho tớ chợt nhặng lên nhăng nhăng , khoảnh khắc lặp đi lặp lại trong não tớ , nó thật đẹp 
     Viết vào ngày 4/5 `,
-
+    
     
 
 
