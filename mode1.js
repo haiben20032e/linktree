82fbf30f6c51f5abc75ca4a6176cc35a0c7cdb33
haiben20032e <doanhai11903@gmail.com>
2025-05-27 02:00:09 +0700

    Cập nhật code mới

--- mode1.js
+++ mode1.js
@@ -1,13 +1,22 @@
 const diaryEntries = [
-    ` 2025 / 1 ngày buồn nào đó
-    Tớ Ghét bản thân tớ , mình chỉ đang cố hằng ngày thức dậy đối mặt với chính bản thân mình chỉ đang cố gắng nỗ lực chứng minh bản thân , tớ chỉ luôn cố gắng làm tốt mọi thứ , nhưng quên cả bản thân mình đang hướng đến điều gì mà lại để đánh mắt thứ quan trọng nhất
-    Chắc tại do mình hướng nội , luôn luôn làm gì cũng chỉ biết tránh né , nói cảm ơn và xin lỗi liên tục vì sợ ảnh hưởng đến người khác 
-    Gặp được người mình yêu hiểu mình nhưng lại tránh né , họ quan tâm nhưng sợ mình không xứng , nỗi tự ti của mình quá lớn chăng , bước vào một mối quan hệ mình đã chỉ để ý đến nhưng thứ tồi tệ nhất , đôi khi mọi thứ thật bình thường nhưng 1 khoảnh khắc tớ làm cậu buồn tớ chợt nghĩ mình không xứng , tự dằn vặt về điều mình đã làm , có lẽ tớ đã vụn vỡ từ những lần trước đó khi mà ng ta bỏ rơi tớ ,lòng tôi vụn vỡ mất 3 năm nó thành 1 vết sẹo , một ám ảnh về tình yêu tự trách 
-    Có lẽ cậu nghĩ tớ rời đi vì "xa" nhưng nếu cần tớ vẫn sẽ tới và ôm cậu thật chặt , xa chưa bao giờ là vấn đề cả tớ còn muốn gặp cậu rất nhiều nữa chứ, cậu là 1 cô gái tốt 1 người hiểu tớ chấp nhận tớ, mọi người bạn bè đạt ấy à nói về cậu tớ chưa bao giờ để ý cả , tớ yêu cậu trong mắt tớ cậu là đẹp nhất đâu quan trọng ng khác nói gì , như ng ta đã nói khi yêu 1 ai họ luôn đẹp nhất , đáng yêu trong mắt của mình - yêu cậu từ 1 khoảnh khắc cơ mà ,cậu vẫn luôn hỏi đó là khoảnh khắc gì ,nó đơn giản lắm "em ăn , em nói ,em cười , thế gian đâu tìm được 2 người như em " chỉ là khoảnh khắc cậu bỗng cười ,cáu bẩn , hay gặp 1 thứ gì đó cute chia sẻ cho tớ chợt nhặng lên nhăng nhăng , khoảnh khắc lặp đi lặp lại trong não tớ , nó thật đẹp 
-    Viết vào ngày 4/5 `,
-    
-    
+   `2025 / 1 ngày buồn nào đó
+Tớ ghét bản thân tớ. Mình chỉ đang cố hằng ngày thức dậy, đối mặt với chính bản thân mình, chỉ đang cố gắng nỗ lực chứng minh bản thân  . Tớ luôn cố gắng làm tốt mọi thứ, nhưng lại quên cả bản thân mình đang hướng đến điều gì, mà để đánh mất thứ quan trọng nhất.
 
+Chắc tại do mình hướng nội, luôn luôn làm gì cũng chỉ biết tránh né, nói cảm ơn và xin lỗi liên tục vì sợ ảnh hưởng đến người khác.
+
+Gặp được người mình yêu, người hiểu mình, nhưng lại tránh né. Họ quan tâm, nhưng mình lại sợ không xứng. Nỗi tự ti của mình quá lớn chăng? Bước vào một mối quan hệ, mình lại chỉ để ý đến những thứ tồi tệ nhất. Đôi khi mọi thứ thật bình thường, nhưng chỉ cần một khoảnh khắc tớ làm cậu buồn, tớ chợt nghĩ mình không xứng, tự dằn vặt vì điều mình đã làm ,mỗi lần nhìn cậu buồn im lặng tớ không nói nhưng tớ đau lắm , tớ luôn sợ làm mọi người xung quanh mà tớ yêu thương buồn , Có lẽ tớ đã vụn vỡ từ những lần trước đó
+
+Có lẽ cậu nghĩ tớ rời đi vì "xa", nhưng nếu cần, tớ vẫn sẽ tới và ôm cậu thật chặt mà . Xa chưa bao giờ là vấn đề cả, tớ còn muốn gặp cậu rất nhiều nữa đó . Cậu là một cô gái tốt, một người hiểu tớ, chấp nhận tớ. Mọi người, bạn bè, Đạt ấy à – nói về cậu tớ chưa bao giờ để ý cả , anh yêu em dù cho e có thế nào. Tớ yêu cậu Trong mắt tớ, cậu luôn là đẹp nhất – đâu cần phải quan trọng người khác nói gì 
+Tớ không biết tớ đã hiểu được cậu phần nào nhưng thật sự khoảng thời gian yêu và đi cùng cậu tớ đã dùng cả trái tim mình để yêu và hiểu cậu ,đừng bận tâm tới lời ng khác “đâu có ai hoàn hảo”
+
+Như người ta đã nói, khi yêu một ai, họ luôn là người đẹp nhất, đáng yêu nhất trong mắt mình. Tớ yêu cậu từ một khoảnh khắc cơ mà. Cậu vẫn luôn hỏi đó là khoảnh khắc gì – nó đơn giản lắm: "em ăn, em nói, em cười, thế gian đâu tìm được 2 người như em".
+
+Chỉ là khoảnh khắc cậu bỗng cười, cáu bẩn, hay gặp một thứ gì đó cute, chia sẻ cho tớ, chợt nhặng lên nhăng nhăng – khoảnh khắc ấy cứ lặp đi lặp lại trong đầu tớ. Nó thật đẹp.
+
+Viết vào ngày 4/5`
+
+    
+   
 
 
 
